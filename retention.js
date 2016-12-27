4d5b8353a15091fca4b3ed78e190ee81468640f9
PillowSky <ihyzi@163.com>
2016-12-27 17:47:34 +0800

    Update retention.js

--- retention.js
+++ retention.js
@@ -1,7 +1,5 @@
 $('table.table .row .span2').each(function(i, e) {
 	$.getJSON('/api/v1/bdc/search/?version=2&word=' + $(e).text(), function(res) {
-		if (res.data.retention == undefined) {
-			$(e).append(' ' + res.data.retention);
-		}
+		$(e).append(' ' + res.data.retention);
 	});
 });
